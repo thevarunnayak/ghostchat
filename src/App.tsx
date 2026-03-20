53ceab20dcf6e8748b5fcf6cd12e9daf29abe9f6
varun.nayak <iamvarunnayak@gmail.com>
2026-03-20 17:21:04 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black">
+      <h1 className="text-4xl font-bold text-green-500">
+        Tailwind is working 🚀
+      </h1>
+    </div>
+  );
+}
